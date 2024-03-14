aa802fcaba90de892b15050f7ade76d320c0b07c
i7eo <admin@i7eo.com>
2024-03-14 18:02:06 +0800

    chore: babel config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  // plugins: [
+  //   "@vue/babel-plugin-jsx",
+  //   {
+  //     mergeProps: false, // 关掉vue自带的合并prop功能，所有的prop传递需要自己明确后进行操作（合并、删除）
+  //   },
+  // ],
   plugins: [
-    "@vue/babel-plugin-jsx",
-    {
-      mergeProps: false, // 关掉vue自带的合并prop功能，所有的prop传递需要自己明确后进行操作（合并、删除）
-    },
+    [
+      "@vue/babel-plugin-jsx",
+      {
+        mergeProps: false, // 关掉vue自带的合并prop功能，所有的prop传递需要自己明确后进行操作（合并、删除）
+      },
+    ],
   ],
 };
